40dde2aa410a0916bfdf956505188e1fd6e81e88
Hugo Menna Barreto <hugombarreto@gmail.com>
2015-07-13 18:54:54 -0700

    Reorganizing file, changing static methods to global function

--- tests/a_star_test.py
+++ tests/a_star_test.py
@@ -1,33 +1,30 @@
 import pickle
 import unittest
 import numpy as np
-from AStarSpecializer.a_star import Node, GridAsArray
+from AStarSpecializer.a_star import GridAsArray
 from AStarSpecializer.specializers import get_specialized_a_star_grid
 
 
 Grid = get_specialized_a_star_grid(GridAsArray)
 # Grid = GridAsArray
 
+def get_random_grid(dimension, barrier_probability=0.3):
+    generate_barriers = np.vectorize(lambda i: np.inf
+        if i < barrier_probability else np.rint(i*1000)+1)
+    grid_array = generate_barriers(np.random.rand(*dimension))
 
-class GraphGenerator(object):
-    barrier_probability = 0.3
+    start = tuple(np.random.random_integers(0, i - 1) for i in dimension)
+    finish = tuple(np.random.random_integers(0, i - 1) for i in dimension)
 
-    @staticmethod
-    def get_random_grid(dimension):
-        generate_barriers = np.vectorize(lambda i: np.inf
-            if i < GraphGenerator.barrier_probability else np.rint(i*1000)+1)
-        grid_array = generate_barriers(np.random.rand(*dimension))
-        start = [np.random.random_integers(0, i - 1) for i in dimension]
-        finish = [np.random.random_integers(0, i - 1) for i in dimension]
-        grid_array[tuple(start)] = 1
-        grid_array[tuple(finish)] = 1
-        return Grid(grid_array), tuple(start), tuple(finish)
+    grid_array[start] = 1
+    grid_array[finish] = 1
+    return Grid(grid_array), start, finish
 
-    @staticmethod
-    def get_grid_from_file(filename):
-        grid, start, finish = pickle.load(open(filename, "rb"))
-        print "file opened"
-        return grid, start, finish
+def get_grid_from_file(filename):
+    grid, start, finish = pickle.load(open(filename, "rb"))
+    print "file opened"
+    return grid, start, finish
+
 
 
 class TestAStar(unittest.TestCase):
@@ -37,12 +34,12 @@ class TestAStar(unittest.TestCase):
         self.graph_size = self.grid_size[0] * self.grid_size[1]
 
     def test_random_grid(self):
-        grid, start, finish = GraphGenerator.get_random_grid(self.grid_size)
+        grid, start, finish = get_random_grid(self.grid_size)
         start_finish_cost = get_a_star_cost(grid, start, finish)
         finish_start_cost = get_a_star_cost(grid, finish, start)
 
-        if start_finish_cost != finish_start_cost:
-            grid.save("random_grid", "boards/", start, finish)
+        # if start_finish_cost != finish_start_cost:
+        #     grid.save("random_grid", "boards/", start, finish)
 
         self.assertEqual(start_finish_cost, finish_start_cost)
 
@@ -54,7 +51,7 @@ class TestAStar(unittest.TestCase):
         import glob
         files = glob.glob("boards/*.p")
         for file_name in files:
-            grid, start, finish = GraphGenerator.get_grid_from_file(file_name)
+            grid, start, finish = get_grid_from_file(file_name)
             self.assertEqual(get_a_star_cost(grid, start, finish),
                              get_a_star_cost(grid, finish, start),
                              "grid from file: " + file_name)
@@ -76,3 +73,6 @@ def get_a_star_cost(graph, start, finish):
         current_node = current_parent
 
     return cost
+
+if __name__ == '__main__':
+    unittest.main()
