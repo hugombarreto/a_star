57e75116a41f475ddb2a559979fec8f687f53eef
Hugo Menna Barreto <hugombarreto@gmail.com>
2015-07-15 17:15:05 -0700

    Integrating the heuristic transformer

--- AStarSpecializer/specializers.py
+++ AStarSpecializer/specializers.py
@@ -10,11 +10,11 @@ from ctree.visitors import NodeTransformer
 from AStarSpecializer.a_star import GridAsArray
 
 import logging
-from AStarSpecializer.generic_transformers import FunctionsLifter, \
-    ClassToStructureTransformer, MethodCallsTransformer
+from AStarSpecializer.generic_transformers import LambdaLifter,\
+    MethodCallsTransformer, ReturnTypeFinder
 from AStarSpecializer.np_functional import TransformFunctionalNP
-from AStarSpecializer.priority_queue_interface import substitute_priority_queue, \
-    PriorityQueueInterface, substitute_node_info
+from AStarSpecializer.priority_queue_interface import transform_priority_queue,\
+    transform_node_info, PriorityQueueInterface
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -60,10 +60,19 @@ class PySpecificAStarConversions(NodeTransformer):
 
     def visit_For(self, node):
         self.generic_visit(node)
+        # FIXME this assumes max num of neighbors is 2 * (num of dimensions)
+        num_neighbors = 2 * self._grid_type._ndim_
+        # call_neighbors = Assign(SymbolRef("neighbors",
+        #                                   ctypes.POINTER(ctypes.c_int)()),
+        #                         node.iter)
+        call_neighbors = [ArrayDef(SymbolRef("neighbors", ctypes.c_int()),
+                                   num_neighbors,
+                                   Array(ctypes.c_int(),
+                                         body=[Constant(-1)]*num_neighbors)),
+                          FunctionCall(SymbolRef(node.iter.func.name),
+                                       [SymbolRef("neighbors"),
+                                        SymbolRef(node.iter.args[0].id)])]
 
-        call_neighbors = Assign(SymbolRef("neighbors",
-                                          ctypes.POINTER(ctypes.c_int)()),
-                                node.iter)
         target_name = node.target.elts[0].id
         target_ref = SymbolRef(target_name)
         index_name = "i"
@@ -73,8 +82,7 @@ class PySpecificAStarConversions(NodeTransformer):
                            self._grid_type._dtype_.type())
         for_loop = For(
             Assign(SymbolRef(index_name, ctypes.c_int()), Constant(0)),
-            # TODO this assumes max num of neighbors is 2 * (num of dimensions)
-            And(Lt(index_ref, Constant(2 * self._grid_type._ndim_)),
+            And(Lt(index_ref, Constant(num_neighbors)),
                 GtE(ArrayRef(SymbolRef("neighbors"), index_ref), Constant(0))),
             PreInc(index_ref),
             [
@@ -94,15 +102,9 @@ def get_func_declarations():
         "self__get_neighbor_weight_list": FunctionDecl(
             return_type=ctypes.POINTER(ctypes.c_int)(),
             name="self__get_neighbor_weight_list",
-            params=[SymbolRef("node", ctypes.c_int())],
+            params=[SymbolRef("neighbors", ctypes.POINTER(ctypes.c_int)()),
+                    SymbolRef("node", ctypes.c_int())],
             defn=[StringTemplate("""return NULL;""")]
-        ),
-        "self__calculate_heuristic_cost": FunctionDecl(
-            return_type=ctypes.c_double(),
-            name="self__calculate_heuristic_cost",
-            params=[SymbolRef("current_node_id", ctypes.c_int()),
-                    SymbolRef("target_node_id", ctypes.c_int())],
-            defn=[StringTemplate("""return 0.0;""")]
         ),
     })
     return dec
@@ -118,73 +120,9 @@ class fixGType(NodeTransformer):
 
     # FIXME return type should be answer
     def visit_Return(self, node):
-        return Return(Constant(0))
-
-
-class ConcreteHeuristicFunction(ConcreteSpecializedFunction):
-    def __init__(self, entry_name, project_node, entry_typesig):
-        self._c_function = self._compile(entry_name, project_node,
-                                         entry_typesig)
-
-    def __call__(self, A):
-        return self._c_function(A)
-
-
-class HeuristicSpecializer(LazySpecializedFunction):
-    def args_to_subconfig(self, args):
-        A = args[0]
-        arg_type = np.ctypeslib.ndpointer(A.dtype, A.ndim, A.shape)
-        return_type = arg_type._dtype_.type
-        return {'arg_type': arg_type, 'return_type': return_type}
-
-    def transform(self, py_ast, program_config):
-        """ This method converts the python AST to C AST
-        :param py_ast:
-        :param program_config:
-        :return:
-        """
-        arg_type = program_config.args_subconfig['arg_type']
-        return_type = program_config.args_subconfig['return_type']
-
-        lift_functions = FunctionsLifter()
-        tree_fn = py_ast.find(ast.FunctionDef, name="apply")
-        for statement in tree_fn.body:
-            lift_functions.visit(statement)
-        lift_functions_list = lift_functions.get_lifted_functions()
-
-        tree = PyBasicConversions().visit(py_ast)
-        tree_fn = tree.find(FunctionDecl, name="apply")
-        tree_fn.return_type = return_type()
-        tree_fn.params[0].type = arg_type()
-
-        functional_np = TransformFunctionalNP(arg_type)
-        tree = functional_np.visit(tree)
-        lift_functions_list += functional_np.specialized_functions
-        tree = lift_functions_list + [tree.body[0]]
-
-        tree = CFile("generated", tree)
-        tree = PyBasicConversions().visit(tree)
-
-        generated_c_file = CFile("generated", tree)
-        return [generated_c_file]
-
-    def finalize(self, transform_result, program_config):
-        # creates a project, transform_result is the return of the
-        # transform method we created above, since we made it a single object,
-        # not a list or a tuple we may use it without indexing
-        project = Project(transform_result)  # Project holds a list of files
-
-        # same as in the above method
-        arg_type = program_config.args_subconfig['arg_type']
-        return_type = program_config.args_subconfig['return_type']
-
-        # this creates a C function prototype that returns something of type
-        # return_type and take one argument of type arg_type, here they are
-        # both the same
-        entry_type = ctypes.CFUNCTYPE(return_type, arg_type)
-
-        return ConcreteHeuristicFunction("apply", project, entry_type)
-
+        if isinstance(node.value, ast.Name):
+            return Return(Constant(np.int32()))
+        return node
 
 class RecursiveSpecializer(object):
     """Specializes a body and all the eventual methods called from it"""
@@ -193,8 +131,8 @@ class RecursiveSpecializer(object):
         self.defines = []
         self.typedef = []
         self.local_include = []
+        self.func_def_names = []
         self.func_defs = []
-        self.func_def_dict = {}
         self.tree = tree
         self.body = None
 
@@ -208,37 +146,43 @@ class RecursiveSpecializer(object):
     def visit(self, body=None):
         if body is None:
             body = self.tree.body[0]
+        #ctree.browser_show_ast(body)
 
-        body = substitute_node_info.visit(body)
-        self.typedef += substitute_node_info.lift
+        body = transform_node_info.visit(body)
+        body = transform_priority_queue.visit(body)
+        for tdef in transform_node_info.lift + transform_priority_queue.lift:
+            if tdef not in self.typedef:
+                self.typedef.append(tdef)
 
-        body = substitute_priority_queue.visit(body)
-        self.typedef += substitute_priority_queue.lift
+        LambdaLifter(self).visit(body)
+
+        coord_type = np.ctypeslib.ndpointer(ctypes.c_int, 1, (self.grid_type._ndim_))
+        TransformFunctionalNP(coord_type, self).visit(body)
 
         dict_to_array_transformer = DictToArrayTransformer(self.grid_type,
                                                            self.typedef)
         body = dict_to_array_transformer.visit(body)
 
-        method_calls_transformer = MethodCallsTransformer(self.func_def_dict,
-                                                          self.self_object,
-                                                          self)
-        body = method_calls_transformer.visit(body)
-        self.func_defs = self.func_def_dict.values() + \
-                         method_calls_transformer.lift
+        MethodCallsTransformer(self).visit(body)
 
         py_specific = PySpecificAStarConversions(self.grid_type)
         body = py_specific.visit(body)
 
         body = fixGType().visit(body)
 
+        return_type_finder = ReturnTypeFinder(self)
+        return_type_finder.visit(body)
+        return_type = return_type_finder.return_type
+
         body = PyBasicConversions().visit(body)
 
         self.body = body
 
+        self.body.return_type = return_type
+
         return body
 
     def get_c_file(self, return_type, param_types):
-        self.body.return_type = return_type
         for param, param_type in zip(self.body.params, param_types):
             param.type = param_type
 
@@ -256,12 +200,11 @@ class AStarSpecializer(LazySpecializedFunction):
         return {'grid_type': grid_type}
 
     def transform(self, py_ast, program_config):
-        #ctree.browser_show_ast(py_ast)
-
         grid_type = program_config.args_subconfig['grid_type']
         specializer = RecursiveSpecializer(py_ast, self.self_object, grid_type)
         specializer.typedef += PriorityQueueInterface.definitions
-        specializer.func_def_dict = get_func_declarations()  # PriorityQueueInterface.functions
+        specializer.func_defs += get_func_declarations().values()
+        specializer.func_def_names += get_func_declarations().keys()
         specializer.includes = [StringTemplate("""\
             #include <stdlib.h>
             #include <math.h>
@@ -271,7 +214,7 @@ class AStarSpecializer(LazySpecializedFunction):
             #define True 1
             #define inf INFINITY
 
-            //#define NUM_DIMENSIONS 3
+            #define NUM_DIMENSIONS 3
             #define GRID_DIMENSIONS {3,4,5}
         """)]
         specializer.local_include = [StringTemplate("""\
@@ -409,13 +352,13 @@ def combine_coordinates(coordinates, grid_dimensions):
     return combined_coordinates
 
 
-def decompose_coordinates(combined_coordinates, grid_dimensions):
+def decompose_coordinates(combined_coordinates, grid_dimensions, offset=0):
     dimension_multiplier = 1
     decomposed_coordinates = []
     for dimension_size in reversed(grid_dimensions):
         new_dimension_multiplier = dimension_multiplier * dimension_size
         multiplied_term = combined_coordinates % new_dimension_multiplier
-        decomposed_coordinates.append(multiplied_term / dimension_multiplier)
+        decomposed_coordinates.append(multiplied_term / dimension_multiplier + offset)
         dimension_multiplier = new_dimension_multiplier
         combined_coordinates -= multiplied_term
 
@@ -437,9 +380,5 @@ def get_specialized_a_star_grid(grid_class):
         return c_a_star(self._grid, start, target, self)
 
     grid_class.a_star = a_star
-
-    # py_heuristic = grid_class._calculate_1_norm
-    # c_heuristic = HeuristicSpecializer.from_function(py_heuristic, 'Heuristic')
-    # grid_class._calculate_1_norm = c_heuristic
 
     return grid_class
