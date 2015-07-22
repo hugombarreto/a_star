ad8931352fe4e29bbc32923f6ecf962b20f3602a
Hugo Menna Barreto <hugombarreto@gmail.com>
2015-07-21 17:42:10 -0700

    Creating constructor for class PriorityQueueInterface

--- a_star/priority_queue_interface.py
+++ a_star/priority_queue_interface.py
@@ -28,74 +28,81 @@ transform_node_info = ClassToStructureTransformer(
 
 
 class PriorityQueueInterface(object):
-    definitions = [StructDef("heap_element", [
-        SymbolRef("id", ctypes.c_int()),
-        SymbolRef("priority", ctypes.c_long())
-    ])]
+    def __init__(self, heap_size, grid_dimensions):
+        self.heap_size = heap_size
+        self.grid_dimensions = grid_dimensions
+        self.num_dimensions = len(grid_dimensions)
 
-    functions = {
-        "PriorityQueue_init": FunctionDecl(
-            return_type=Struct("PriorityQueue", ptr=True),
-            name="PriorityQueue_init",
-            params=None,
-            # FIXME properly determine the heap size
-            defn=[Return(FunctionCall(SymbolRef("new_heap"), [Constant(10)]))]
-        ),
-        "PriorityQueue_push": FunctionDecl(
-            return_type=None,
-            name="PriorityQueue_push",
-            params=[SymbolRef("queue", Struct("PriorityQueue", ptr=True)),
-                    SymbolRef("element", Struct("heap_element"))],
-            defn=[StringTemplate("""heap_insert(queue, element);""")]
-        ),
-        "PriorityQueue_pop": FunctionDecl(
-            return_type=Struct("heap_element"),
-            name="PriorityQueue_pop",
-            params=[SymbolRef("queue", Struct("PriorityQueue", ptr=True))],
-            defn=[StringTemplate("""\
-                struct heap_element element = *find_heap_min(queue);
-                delete_heap_min(queue);
-                return element;""")
-                  ]
-        ),
-        "Node": FunctionDecl(
-            return_type=Struct("heap_element"),
-            name="Node",
-            params=[SymbolRef("id", ctypes.c_int()),
-                    SymbolRef("priority", ctypes.c_long())],
-            defn=[StringTemplate("""\
-                struct heap_element element;
-                element.id = id;
-                element.priority = priority;
-                return element;""")
-                  ]
-        ),
-        "np_array": FunctionDecl(  # FIXME memory leak by returning pointer
-            return_type=ctypes.POINTER(ctypes.c_int)(),
-            name="np_array",
-            params=[SymbolRef("coordinate", ctypes.c_int())],
-            defn=[StringTemplate("""\
-                int dimension_multiplier = 1;
-                int grid_dimensions[] = GRID_DIMENSIONS;
-                int* multiple_coordinate;
-                if ((multiple_coordinate = malloc(NUM_DIMENSIONS * sizeof(*multiple_coordinate))) == NULL) {
-                    return NULL;
-                }
+        self.definitions = [StructDef("heap_element", [
+            SymbolRef("id", ctypes.c_int()),
+            SymbolRef("priority", ctypes.c_long())
+        ])]
 
-                for (int i = 0; i < NUM_DIMENSIONS; ++i) {
-                    int new_dimension_multiplier = dimension_multiplier * grid_dimensions[i];
-                    int multiplied_term = coordinate % new_dimension_multiplier;
-                    multiple_coordinate[i] = multiplied_term / dimension_multiplier;
-                    dimension_multiplier = new_dimension_multiplier;
-                    coordinate -= multiplied_term;
-                }
-                return multiple_coordinate;
-            """)]
-        ),
-        "len": FunctionDecl(
-            return_type=np.uintc(),
-            name="len",
-            params=[SymbolRef("queue", Struct("PriorityQueue", ptr=True))],
-            defn=[StringTemplate("""return queue->size;""")]
-        ),
-    }
+        self.functions = {
+            "PriorityQueue_init": FunctionDecl(
+                return_type=Struct("PriorityQueue", ptr=True),
+                name="PriorityQueue_init",
+                params=None,
+                defn=[Return(FunctionCall(SymbolRef("new_heap"),
+                                          [Constant(self.heap_size)]))]
+            ),
+            "PriorityQueue_push": FunctionDecl(
+                return_type=None,
+                name="PriorityQueue_push",
+                params=[SymbolRef("queue", Struct("PriorityQueue", ptr=True)),
+                        SymbolRef("element", Struct("heap_element"))],
+                defn=[StringTemplate("""heap_insert(queue, element);printf("%i\\n", queue->size);""")]
+            ),
+            "PriorityQueue_pop": FunctionDecl(
+                return_type=Struct("heap_element"),
+                name="PriorityQueue_pop",
+                params=[SymbolRef("queue", Struct("PriorityQueue", ptr=True))],
+                defn=[StringTemplate("""\
+                    struct heap_element element = *find_heap_min(queue);
+                    delete_heap_min(queue);
+                    return element;""")
+                      ]
+            ),
+            "Node": FunctionDecl(
+                return_type=Struct("heap_element"),
+                name="Node",
+                params=[SymbolRef("id", ctypes.c_int()),
+                        SymbolRef("priority", ctypes.c_long())],
+                defn=[StringTemplate("""\
+                    struct heap_element element;
+                    element.id = id;
+                    element.priority = priority;
+                    return element;""")
+                      ]
+            ),
+            "np_array": FunctionDecl(  # FIXME memory leak by returning pointer
+                return_type=ctypes.POINTER(ctypes.c_int)(),
+                name="np_array",
+                params=[SymbolRef("coordinate", ctypes.c_int())],
+                defn=[StringTemplate("""\
+                    int dimension_multiplier = 1;
+                    int grid_dimensions[] = $GRID_DIMENSIONS;
+                    int* multiple_coordinate;
+                    if ((multiple_coordinate = malloc($NUM_DIMENSIONS * sizeof(*multiple_coordinate))) == NULL) {
+                        return NULL;
+                    }
+
+                    for (int i = 0; i < $NUM_DIMENSIONS; ++i) {
+                        int new_dimension_multiplier = dimension_multiplier * grid_dimensions[i];
+                        int multiplied_term = coordinate % new_dimension_multiplier;
+                        multiple_coordinate[i] = multiplied_term / dimension_multiplier;
+                        dimension_multiplier = new_dimension_multiplier;
+                        coordinate -= multiplied_term;
+                    }
+                    return multiple_coordinate;
+                """,{'GRID_DIMENSIONS': Array(body=[Constant(i) for i in
+                                                    self.grid_dimensions]),
+                     'NUM_DIMENSIONS': Constant(self.num_dimensions)})]
+            ),
+            "len": FunctionDecl(
+                return_type=np.uintc(),
+                name="len",
+                params=[SymbolRef("queue", Struct("PriorityQueue", ptr=True))],
+                defn=[StringTemplate("""return queue->size;""")]
+            ),
+        }
