f036954c097496f12280440faaa4b89ccb034fec
Hugo Menna Barreto <hugombarreto@gmail.com>
2015-08-03 13:59:43 -0700

    Instructions on how to run the time comparison

--- README.rst
+++ README.rst
@@ -1,5 +1,11 @@
-a_star -- A specializer built on the ASP SEJITS framework
+a_star -- An A* specializer built using ctree
 -------
+
+To run a time comparison between the Python and Ctree implementation, you can
+use the TimeComparison class. The ``main.py`` file is set to use it, just run::
+
+    python a_star/main.py
+
 
 AStarSpecializer is based on ctree
 for more information on ctree see `ctree on github <http://github.com/ucb-sejits/ctree>`_.
--- a_star/main.py
+++ a_star/main.py
@@ -1,16 +1,12 @@
 """
 specializer AStarSpecializer
 """
-from a_star.specializers import AStarSpecializer
 
-
-def mul_3(i):
-    return i*3
-
+import unittest
+from tests.time_comparison import TimeComparison
 
 if __name__ == '__main__':
-    c_mul_3 = AStarSpecializer.from_function(mul_3, 'Translator')
+    suite = unittest.TestSuite()
+    suite.addTest(TimeComparison('test_growing_size'))
 
-    print mul_3(2)
-    print c_mul_3(2)
-
+    unittest.TextTestRunner(verbosity=1).run(suite)
